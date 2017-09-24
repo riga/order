2faefdb2135c268a1c49034b545ca5336f13e020
M. Rieger <marcelrieger@me.com>
2017-09-24 18:17:03 +0200

    Add Process class with tests and docs.

--- order/process.py
+++ order/process.py
@@ -10,10 +10,103 @@ __all__ = ["Process"]
 
 from scinum import Number
 
-from .unique import UniqueObject
+from .unique import UniqueObject, unique_tree
+from .mixins import AuxDataMixin, DataSourceMixin, LabelMixin, ColorMixin
+from .util import typed
 
 
-class Process(UniqueObject):
+@unique_tree(plural="processes")
+class Process(UniqueObject, AuxDataMixin, DataSourceMixin, LabelMixin, ColorMixin):
+    """ __init__(name, id, xsecs=None, color=None, label=None, label_short=None, is_data=False, aux=None, context=None)
+    Definition of a phyiscs process.
+
+    *xsecs* should be a mapping of center-of-mass energy to cross section (a *scinum.Number*
+    instance). *color* is forwarded to the :py:class:`ColorMixin`, *label* and *label_short* to the
+    :py:class:`LabelMixin`, *is_data* to the :py:class:`DataSourceMixin`, *aux* to the
+    :py:class:`AuxDataMixin`, and *name*, *id* and *context* to the :py:class:`UniqueObject`
+    constructor.
+
+    A process can have parent-child relations to other processes.
+
+    .. code-block:: python
+
+        from scinum import Number
+
+        p = Process("ttH", 1,
+            xsecs = {
+                13: Number(0.5071, {"scale": (Number.REL, 0.036)})
+            },
+            label = r"$t\\bar{t}H$",
+            color = (255, 0, 0)
+        )
+
+        p.get_xsec(13)
+        # -> "0.51, scale: (+0.02, -0.02)"
+
+        p.label_root
+        # -> "t#bar{t}H"
+
+        p.add_process("ttH_bb", 2,
+            xsecs = {
+                13: p.get_xsec(13) * 0.5824
+            },
+            label = p.label + r", $b\\bar{b}$"
+        )
+
+        p.get_process("ttH_bb").label_root
+        # -> "t#bar{t}H, b#bar{b}"
+
+    .. py:attribute:: xsecs
+       type: dictionary (float -> scinum.Number)
+
+       Cross sections mapped to a center-of-mass energy with arbitrary units.
     """
-    TODO.
-    """
+
+    def __init__(self, name, id, xsecs=None, color=None, label=None, label_short=None,
+                 is_data=False, aux=None, context=None):
+        UniqueObject.__init__(self, name, id, context=context)
+        AuxDataMixin.__init__(self, aux=aux)
+        DataSourceMixin.__init__(self, is_data=is_data)
+        LabelMixin.__init__(self, label=label, label_short=label_short)
+        ColorMixin.__init__(self, color=color)
+
+        # instance members
+        self._xsecs = {}
+
+        # set initial values
+        if xsecs is not None:
+            self.xsecs = xsecs
+
+    @typed
+    def xsecs(self, xsecs):
+        # xsecs parser
+        try:
+            xsecs = dict(xsecs)
+        except:
+            raise TypeError("invalid xsecs type: %s" % xsecs)
+
+        # parse particular values
+        _xsecs = {}
+        for ecm, xsec in xsecs.items():
+            if not isinstance(ecm, (int, float)):
+                raise TypeError("invalid xsec energy type: %s" % ecm)
+            elif not isinstance(xsec, Number):
+                try:
+                    xsec = Number(xsec)
+                except:
+                    raise TypeError("invalid xsec value type: %s" % xsec)
+            _xsecs[float(ecm)] = xsec
+
+        return _xsecs
+
+    def get_xsec(self, ecm):
+        """
+        Returns the cross section *Number* for a center-of-mass energy *ecm*.
+        """
+        return self.xsecs[ecm]
+
+    def set_xsec(self, ecm, xsec):
+        """
+        Sets the *xsec* *Number* for a center-of-mass energy *ecm*.
+        """
+        self.xsecs.update(self.__class__.xsecs.fparse({ecm: xsec}))
--- tests/test_process.py
+++ tests/test_process.py
@@ -7,9 +7,32 @@ __all__ = ["ProcessTest"]
 import unittest
 
 from order import Process
+from scinum import Number
 
 
 class ProcessTest(unittest.TestCase):
 
     def test_constructor(self):
-        p = Process("ttbar", 1)
+        p = Process("ttH", 1,
+            xsecs = {
+                13: Number(0.5071, {"scale": (Number.REL, 0.036)})
+            },
+            label = r"$t\bar{t}H$",
+            color = (255, 0, 0)
+        )
+
+        self.assertEqual(p.get_xsec(13).n, 0.5071)
+        self.assertEqual(p.label_root, "t#bar{t}H")
+
+        with self.assertRaises(TypeError):
+            Process("ttH_bb", 2, xsecs="nodict")
+
+        with self.assertRaises(TypeError):
+            Process("ttH_cc", 3, xsecs={13: ""})
+
+    def test_child_processes(self):
+        p = Process("tt", 4)
+        p5 = p.add_process("tt_bb", 5)
+
+        self.assertEqual(p.get_process(5), p5)
+        self.assertEqual(p5.get_parent_process(4), p)

d6268b190e6ddb6af282e29f299dff21f0c61ef1
Peter Fackeldey <peter.fackeldey@rwth-aachen.de>
2020-10-27 11:46:41 +0100

    Add a small function for pretty print od.Processes including cross-sections and Process hierarchies

--- order/util.py
+++ order/util.py
@@ -372,3 +372,12 @@ class DotAccessProxy(object):
                 raise Exception("cannot set attribute, setter not defined on {}".format(
                     self.__class__.__name__))
             setter(attr, value)
+            
+            
+def pprint_processes(xs_key=13):
+    todo = [(p, 0) for p in od.Process._instances.values() if p.is_root_process]
+    while todo:
+        proc, depth = todo.pop(0)
+        prefix = ("" if depth else "\n") + "| " * depth + "> "
+        print("%-50s %s" % (prefix + proc.name, proc.xsecs.get(xs_key, None)))
+        todo = [(p, depth + 1) for p in proc.processes.values()] + todo

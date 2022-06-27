37788991335fb1da6ac1e1eab7b2161bdb31ede2
Marcel R <marcelrieger@me.com>
2022-06-27 11:43:53 +0200

    Version 1.3.5.

--- order/__version__.py
+++ order/__version__.py
@@ -6,9 +6,9 @@ Pythonic class collection that helps you structure external data from LHC / HEP 
 
 __author__ = "Marcel Rieger"
 __email__ = "python-order@googlegroups.com"
-__copyright__ = "Copyright 2018-2021, Marcel Rieger"
+__copyright__ = "Copyright 2018-2022, Marcel Rieger"
 __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/order"
 __license__ = "BSD-3-Clause"
 __status__ = "Development"
-__version__ = "1.3.4"
+__version__ = "1.3.5"

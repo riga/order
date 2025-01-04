6ea84f9a159437b015b0a75de46ee3fe76f36998
Marcel Rieger <riga@users.noreply.github.com>
2025-01-04 20:25:26 +0100

    License.

--- order/__version__.py
+++ order/__version__.py
@@ -6,7 +6,7 @@ Pythonic class collection that helps you structure external data from LHC / HEP 
 
 __author__ = "Marcel Rieger"
 __email__ = "github.riga@icloud.com"
-__copyright__ = "Copyright 2018-2024, Marcel Rieger"
+__copyright__ = "Copyright 2018-2025, Marcel Rieger"
 __credits__ = ["Marcel Rieger"]
 __contact__ = "https://github.com/riga/order"
 __license__ = "BSD-3-Clause"
